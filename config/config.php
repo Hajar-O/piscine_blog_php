b284712c74ddefdfb93e279f3c617edfa2f9609c
Hajar-O <hajar.ouhadi@lapiscine.pro>
2024-07-16 14:35:02 +0200

    ajout commentaires

--- config/config.php
+++ config/config.php
@@ -8,26 +8,31 @@ error_reporting(E_ALL);
 
 session_set_cookie_params(30);
 
-
+//  création d'une nouvelle classe BDconnection
 class DBconnection {
     private $dsn;
 
     private $username;
 
     private $password;
-
+// Le __constructe va etre appelé à chaque création d'une nouvelle instance.
     function __construct(){
         $this -> dsn = 'mysql:host=localhost:8889;dbname=piscine_blog_php';
         $this -> username = 'root';
         $this -> password = 'root';
     }
 
+    // La méthode connect sert à tester la connection à la bbd.
     function connect(){
+        // try : création d'une nouvelle insentence de class pour tester les identifiants du localhost grace au constructeur du PDO.
         try {
             $pdo = new PDO($this -> dsn, $this -> username, $this -> password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             return $pdo;
+
+            // catch : si la connexion n'a pas abouti, on récupère l'erreur et envoie le massage d'erreur.
+
         } catch (PDOException $e) {
             echo "Erreur de connexion : " . $e->getMessage();
         }
