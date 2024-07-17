2c416c8fc8b12fa7e7c1cde3170f1c77fc377705
Hajar-O <hajar.ouhadi@lapiscine.pro>
2024-07-17 10:03:06 +0200

    ajout commentaires

--- controller/indexController.php
+++ controller/indexController.php
@@ -5,10 +5,11 @@ require_once("../model/articleRepository.php");
 class indexController {
 
     public function index(){
-
+// j'instencie une nouvelle classe ArticleRepository
         $articleRepository = new ArticleRepository();
+        // j'appelle la méthode qui va afficher tous les articles.
         $articles = $articleRepository -> findArticle();
-    
+    // j'appelle la vue qui affiche le HTML
         require_once("../templates/page/indexView.php");
     }
 };
--- model/articleRepository.php
+++ model/articleRepository.php
@@ -3,10 +3,11 @@
 class ArticleRepository {
 
     public function findArticle(){
-
+// j'intencie une nouvelle classe BDconnection
         $DBconnection = new DBconnection();
+        // j'appelle la méthode qui permet la connexion à la DBB.
         $pdo = $DBconnection -> connect();
-
+// j'utilise le Query() qui permet de faite une requête à la base de données et selectionne toutes colonnes de la table article.
         $stmt = $pdo->query("SELECT * FROM article");
         // retourne dans un tableau tous les produits.
         $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
