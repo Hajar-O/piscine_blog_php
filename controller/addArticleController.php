38e7763ff2d62e26cb1273275ac524062b286e97
Hajar-O <hajar.ouhadi@lapiscine.pro>
2024-07-17 13:52:47 +0200

    refactorisation_controller

--- controller/addArticleController.php
+++ controller/addArticleController.php
@@ -1,13 +1,11 @@
 <?php
 
 require_once ("../config/config.php");
+require_once ("../model/articleRepository.php");
 class AddArticleController{
 
-    public function addArticle()
-    {
-        // j'établie la connexion à la BDD.
-        $DBconnection = new DBconnection();
-        $pdo = $DBconnection->connect();
+    public function addArticle(){
+
 
 
         // Définir les paramètres et exécuter
@@ -15,24 +13,10 @@ class AddArticleController{
         $content = " lorem ipsum la piscine blabla blabla";
         $date = "2024-07-17";
 
-        // Préparer la requête d'insertion. la vérification des données fournis par l'utilisateur est fait avec le 'prepare' et 'bindParam'
-        $sql = "INSERT INTO article (title, content, created_at) VALUES (:title, :content, :created_at)";
-        $stmt=$pdo->prepare($sql);
+        $ArticleRepository = new ArticleRepository();
+        $ArticleRepository ->insert($title, $content, $date);
 
 
-// on remplace les paramètres par les vraies valeurs:
-
-        // On réalise l'opération sur plusieurs étapes pour éviter les injections externieurs SQL.
-        $stmt->bindParam(':title', $title);
-        $stmt->bindParam(':content', $content);
-        $stmt->bindParam(':created_at', $date);
-
-// Exécuter la requête
-        if ($stmt->execute()){
-            echo "Nouvel article ajouté avec succès";
-        } else {
-            echo "Erreur lors de l'ajout de l'article";
-        }
     }
 }
 
--- model/articleRepository.php
+++ model/articleRepository.php
@@ -14,4 +14,30 @@ class ArticleRepository {
 
         return $articles;
     }
+    public function insert ($title,$content, $date){
+
+        // j'établie la connexion à la BDD.
+        $DBconnection = new DBconnection();
+        $pdo = $DBconnection->connect();
+
+        // Préparer la requête d'insertion. la vérification des données fournis par l'utilisateur est fait avec le 'prepare' et 'bindParam'
+        $sql = "INSERT INTO article (title, content, created_at) VALUES (:title, :content, :created_at)";
+        $stmt=$pdo->prepare($sql);
+
+
+        // on remplace les paramètres par les vraies valeurs:
+
+        // On réalise l'opération sur plusieurs étapes pour éviter les injections externieurs SQL.
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':created_at', $date);
+
+        // Exécuter la requête
+        if ($stmt->execute()){
+            echo "Nouvel article ajouté avec succès";
+        } else {
+            echo "Erreur lors de l'ajout de l'article";
+        }
+
+    }
 };
